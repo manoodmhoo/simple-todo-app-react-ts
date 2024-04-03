c4762fce262dc74fd19110cc26249ca65cb2684f
Sahaparp Seekaewsiw <sahaparp.se@gmail.com>
2024-04-03 22:17:49 +0700

    init project

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+import {  Text } from '@geist-ui/core'
+
+function App() {
+
+  return (
+    <>
+      <Text h1>Simple Todo App</Text>
+    </>
+  )
+}
+
+export default App
